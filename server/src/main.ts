ab75213edc62aed254ad74f11157797357661b65
kate777 <ekaterinapranyuk1@gmail.com>
2026-09-12 23:53:45 +0300

    fix: allow cors

--- server/src/main.ts
+++ server/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: ['https://expense-tracker-kate18.vercel.app', 'http://localhost:5173'],
     credentials: true,
   });
 
